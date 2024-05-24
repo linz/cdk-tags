1163e6595a014253f95f1d2ba99d61762e912fb0
Bharath <cippy.bharath@gmail.com>
2024-05-24 12:54:01 +1200

    feat: Added tag validations and updated impact tag values
    the responderTeam tag values are derived from the current opsgenie team values. in the future we may automatically generate these values. for now it is a point in time list of values.

---
+++ src/responder-teams.ts
@@ -0,0 +1,16 @@
+export type ResponderTeam= 'LANDONLINE - Common Services' |
+    'LANDONLINE - Datacom' |
+    'LANDONLINE - Dealings' |
+    'LANDONLINE - Enablement' |
+    'LANDONLINE - L2 Technical Support' |
+    'LANDONLINE - Legacy Landonline' |
+    'LANDONLINE - Notice of Change' |
+    'LANDONLINE - Search' |
+    'LANDONLINE - Survey External' |
+    'LANDONLINE - Survey Internal' |
+    'LANDONLINE - Titles Internal' |
+    'LINZ - Enterprise Platforms' |
+    'LINZ-BASEMAPS' |
+    'LINZ-BASEMAPS-dev' |
+    'TESTING L2 Escalation' |
+    'TESTING L2 Support' ;
