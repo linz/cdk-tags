c0b4db322be05beb340a9ad5c8b973539c3d6fb7
Srinika Gunawardane <sgunawardane@linz.govt.nz>
2025-08-20 20:00:07 +1200

    refactor: enhance tags and responder team types for improved type safety (#157)
    ### Motivation
    
    Export the responder and impact as consts so its type can be used in Zod
    directly
    
    ### Modifications
    
    Updated the ResponderTeam and Impact types with ResponderTeams and Impacts string arrays newly exported
    
    ### Verification

--- src/responder-teams.ts
+++ src/responder-teams.ts
@@ -1,14 +1,16 @@
-export type ResponderTeam =
-  | 'LANDONLINE - Common Services'
-  | 'LANDONLINE - Datacom'
-  | 'LANDONLINE - Dealings'
-  | 'LANDONLINE - Enablement'
-  | 'LANDONLINE - L2 Technical Support'
-  | 'LANDONLINE - Legacy Landonline'
-  | 'LANDONLINE - Search'
-  | 'LANDONLINE - Survey External'
-  | 'LANDONLINE - Titles Internal'
-  | 'LI - Basemaps'
-  | 'LI - Geospatial Data Engineering'
-  | 'LINZ - Enterprise Platforms'
-  | 'LINZ - Security';
+export const ResponderTeams = [
+  'LANDONLINE - Common Services',
+  'LANDONLINE - Datacom',
+  'LANDONLINE - Dealings',
+  'LANDONLINE - Enablement',
+  'LANDONLINE - L2 Technical Support',
+  'LANDONLINE - Legacy Landonline',
+  'LANDONLINE - Search',
+  'LANDONLINE - Survey External',
+  'LANDONLINE - Titles Internal',
+  'LI - Basemaps',
+  'LI - Geospatial Data Engineering',
+  'LINZ - Enterprise Platforms',
+  'LINZ - Security',
+] as const;
+export type ResponderTeam = (typeof ResponderTeams)[number];
