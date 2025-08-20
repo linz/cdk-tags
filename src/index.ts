6264878405334bdfc21e8643067fec0da65b86ec
Srinika Gunawardane <sgunawardane@linz.govt.nz>
2025-08-20 14:30:36 +1200

    refactor: export ResponderTeams and Impacts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export { Backup, BackupSchedule } from './backup.js';
 export { getGitBuildInfo } from './build.js';
 export { CommonDataTags } from './data.js';
-export { ResponderTeam } from './responder-teams.js';
+export { ResponderTeam, ResponderTeams } from './responder-teams.js';
 export { SecurityClassification } from './security.js';
-export type { Impact, TagsBase } from './tags.js';
+export type { Impact, Impacts, TagsBase } from './tags.js';
 export { applyTags, applyTagsData } from './tags.js';
