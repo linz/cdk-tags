f6e6b861d8ff2859740b314986bfd89e9845d561
Srinika Gunawardane <sgunawardane@linz.govt.nz>
2025-08-20 13:13:51 +1200

    refactor: reorder exports for linting

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export { Backup, BackupSchedule } from './backup.js';
 export { getGitBuildInfo } from './build.js';
 export { CommonDataTags } from './data.js';
-export { SecurityClassification } from './security.js';
-export type { TagsBase, Impact } from './tags.js';
-export { applyTags, applyTagsData } from './tags.js';
 export { ResponderTeam } from './responder-teams.js';
+export { SecurityClassification } from './security.js';
+export type { Impact, TagsBase } from './tags.js';
+export { applyTags, applyTagsData } from './tags.js';
