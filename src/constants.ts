19a4563eceb3b4ad5372543b453a52470c1f1569
Eugene Dementyev <ekini@users.noreply.github.com>
2026-01-13 13:59:11 +1300

    feat: ENABLE-5403: export tag keys as constants (#199)
    ### Motivation
    
    Export tag keys as constants so that they can be used in our aws cdk
    packages
    
    <!-- TODO: Say why you made your changes. -->
    
    ### Modifications
    
    <!-- TODO: Say what changes you made. -->
    
    <!-- TODO: Attach screenshots if you changed the UI. -->
    
    ### Verification
    
    <!-- TODO: Say how you tested your changes. -->

---
+++ src/constants.ts
@@ -0,0 +1,39 @@
+/**
+ * Tag key constants used throughout the application
+ */
+export const TagKeys = {
+  // Application tags
+  APP_NAME: 'linz.app.name',
+  APP_VERSION: 'linz.app.version',
+  APP_IMPACT: 'linz.app.impact',
+
+  // Environment tags
+  ENVIRONMENT: 'linz.environment',
+
+  // Ownership tags
+  GROUP: 'linz.group',
+  RESPONDER_TEAM: 'linz.responder.team',
+
+  // Git tags
+  GIT_HASH: 'linz.git.hash',
+  GIT_REPOSITORY: 'linz.git.repository',
+
+  // Build tags
+  BUILD_ID: 'linz.build.id',
+
+  // Security tags
+  SECURITY_CLASSIFICATION: 'linz.security.classification',
+
+  // Data tags
+  DATA_ROLE: 'linz.data.role',
+  DATA_IS_MASTER: 'linz.data.is-master',
+  DATA_IS_PUBLIC: 'linz.data.is-public',
+
+  // Backup tags
+  BACKUP_ENABLED: 'linz.backup.enabled',
+  BACKUP_RETENTION: 'linz.backup.retention',
+  BACKUP_SCHEDULE: 'linz.backup.schedule',
+
+  // Logging tags
+  LOGS_STREAMING_FILTER_PATTERN: 'linz.logs.streaming-filter-pattern',
+} as const;
