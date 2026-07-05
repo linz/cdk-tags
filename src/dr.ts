871a31dd9862ef10c3bdd328b662491c49ed22db
Manochitra-Loganathan <MLoganathan@linz.govt.nz>
2026-07-06 08:51:01 +1200

    feat: add DR orchestration tags

---
+++ src/dr.ts
@@ -0,0 +1,5 @@
+/** Tag to identify the DR validation group. */
+export const ArcDrGroupNameTag = 'arc-dr-validation-group';
+
+/** Tag to identify the IAM role ARN to be assumed for DR validation. */
+export const ArcDrValidationRoleArnTag = 'arc-dr-validation-role-arn';
